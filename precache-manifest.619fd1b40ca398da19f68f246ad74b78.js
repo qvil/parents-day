988032b545f477872aa1271621090e01e7a77a2e
Taesu Hyeon <qvil1127@gmail.com>
2019-05-03 00:57:29 +0900

    Updates

---
+++ precache-manifest.619fd1b40ca398da19f68f246ad74b78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c6419eb04bf39005d5cf9431b7924a",
+    "url": "/parents-day/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/parents-day/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c93f9f55025c0905cc06",
+    "url": "/parents-day/static/js/main.a436bd89.chunk.js"
+  },
+  {
+    "revision": "f1e44128a82f5928b5b2",
+    "url": "/parents-day/static/js/runtime~main.af11c7c4.js"
+  }
+]);
